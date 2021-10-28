e34ea01d60ca30148a5639f14ba226a5578e888d
Niraj Gurung <nirajguroong363@gmail.com>
2021-10-28 09:47:02 +0545

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="addTask()" class="btn" :style="{ background: color }">
+        {{ text }}
+    </button>
+</template>
+
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        addTask() {
+            this.$emit('btn-click')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>
+            {{ task.text }}
+
+            <i class="fas fa-times" @click="$emit('delete-task',task.id)"></i>
+        </h3>
+
+        <p>{{ task.day }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    }
+}
+</script>
+
+<style scoped>
+    .task {
+        background: #dadada;
+        padding: 10px 20px;
+        margin: 5px;
+        text-align: left;
+    }
+
+    .task h3 {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .fas {
+        color: red;
+    }
+
+    .task.reminder {
+        border-left: 4px solid green;
+    }
+</style>
